9127576aa0a08cf8bd6864e944c667377146e7f7
Vinícius <euphonion@tutanota.com>
2021-09-19 23:51:03 -0300

    Script javascript

--- src/script/javascript.js
+++ src/script/javascript.js
@@ -1,4 +1,1 @@
-<script>
-	  //caixa de alerta que abre ao acessar a home do site
-		alert ("Olá! Somos a IGV Developer. Conheça nosso trabalho!");
-</script>
+window.alert("Olá! Somos a IGV Developer. Conheça nosso trabalho!");
