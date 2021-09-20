7231485d3f85f38780cccee118fdacdc8c493f83
Isadora★ ^_^ <90213408+isakura4@users.noreply.github.com>
2021-09-19 23:21:25 -0300

    Update javascript.js

--- src/script/javascript.js
+++ src/script/javascript.js
@@ -1,13 +1,1 @@
-<html>  
-   <head>     
-      <script type = "text/javascript">  
-            function fun() {  
-               alert("Olá");  
-            }  
-      </script>       
-   </head> 
 
-function fun() { 
-alert ("Olá!");
-  
-}
